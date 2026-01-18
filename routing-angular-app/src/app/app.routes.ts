21f9c7b0694cb442c95da5deafb6f799dad4b5bc
unknown <raghuvarbisht@gmail.com>
2026-01-18 14:24:42 +0530

    routing inital commit

--- routing-angular-app/src/app/app.routes.ts
+++ routing-angular-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', component:HomeComponent},
+{path: 'about', component:AboutComponent},
+{path: 'login', component:LoginComponent},
+{path: 'contact', component:ContactComponent}
+
+];
